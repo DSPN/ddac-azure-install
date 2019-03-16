e11e62d10159fe7677aa6d881d443917a05b5aed
Scott Hendrickson <scott.hendrickson@datastax.com>
2019-03-16 13:38:58 -0400

    adding waits

--- dse-init.sh
+++ dse-init.sh
@@ -11,8 +11,10 @@ mkdir /usr/share/dse
 chown cassandra:cassandra /usr/share/dse
 cd /usr/share/dse
 # install DDAC
-tar -xvf /home/ddac/ddac-5.1.12-bin.tar.gz  --strip-components=1
-sleep 10
+tar -xvf /home/ddac/ddac-5.1.12-bin.tar.gz  --strip-components=1 &
+tar_process_id=$!
+wait $tar_process_id
+
 chown -R cassandra:cassandra /usr/share/dse
 # no seeds being passed in would be bad - default to private ip
 if [ -z "$seeds" ]
@@ -20,10 +22,17 @@ then
    seeds=`echo $(hostname -I)`
 fi
 privip=`echo $(hostname -I)`
+
 # set cassandra.yaml properties
-/home/ddac/ddac-azure-install/dse-vm-cassandra-props.sh $seeds $cluster_name $privip
+/home/ddac/ddac-azure-install/dse-vm-cassandra-props.sh $seeds $cluster_name $privip &
+cprops_process_id=$!
+wait $cprops_process_id
+
 # set cassandra-rackdc.yaml properties
-/home/ddac/ddac-azure-install/dse-vm-rack-props.sh $dc 
+/home/ddac/ddac-azure-install/dse-vm-rack-props.sh $dc &
+rack_process_id=$!
+wait $rack_process_id
+
 # 
 # update env
 sed -e 's|PATH="\(.*\)"|PATH="/usr/share/dse/bin:/usr/share/dse/tools/bin:\1"|g' -i /etc/environment
@@ -31,8 +40,11 @@ sed -e 's|PATH="\(.*\)"|PATH="/usr/share/dse/bin:/usr/share/dse/tools/bin:\1"|g'
 
 # start DDAC on node
 cp /home/ddac/ddac-azure-install/cassandra.service /etc/systemd/system
-systemctl start cassandra
-sleep 20
+
+systemctl start cassandra &
+cassstart_process_id=$!
+wait $cassstart_process_id
+
 echo "cassandra started"
 systemctl enable cassandra
 echo "dse-init ------> deploy-dse exit status $?"
